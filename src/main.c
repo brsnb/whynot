244feb6369b23ae53a980fff628a7043755cba19
brsnb <49287229+brsnb@users.noreply.github.com>
2021-04-22 21:32:25 -0700

    move to glfw

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
-#include "log.h"
-#include <stdio.h>
+#include <assert.h>
 #include <vulkan/vulkan.h>
-#include <vulkan/vulkan_xcb.h>
-#include <xcb/xcb.h>
+#include <GLFW/glfw3.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/types.h>
 #define STB_DS_IMPLEMENTATION
+#define STBDS_NO_SHORT_NAMES
 #include "stb_ds.h"
 #include "util.h"
+#include "log.h"
 
 #define NAME "whynot"
 #define VK_API_VERSION VK_API_VERSION_1_2
@@ -14,12 +17,100 @@
 #define DEBUG_ENABLED
 #endif
 
-struct _Core {
+struct _WnCore {
     VkInstance instance;
     VkDebugUtilsMessengerEXT debug_messenger;
 } core;
 
-void wn_init_vulkan(void)
+typedef struct {
+    uint32_t compute;
+    uint32_t graphics;
+    uint32_t present;
+    uint32_t transfer;
+    VkQueueFlags supported;
+} WnQueueFamilyIndices;
+
+typedef struct {
+    VkQueue compute;
+    VkQueue graphics;
+    VkQueue present;
+    VkQueue transfer;
+} WnQueues;
+
+struct _WnDevice {
+    VkDevice logical;
+    VkPhysicalDevice physical;
+    VkPhysicalDeviceProperties properties;
+    VkPhysicalDeviceFeatures features;
+    WnQueueFamilyIndices queue_family_indices;
+    WnQueues queues;
+} device;
+
+struct _Swapchain {
+    VkSurfaceKHR surface;
+    VkSurfaceFormatKHR surface_formats;
+    VkExtent2D surface_extent;
+} swapchain;
+
+
+void wn_init_swapchain(GLFWwindow* window) {
+    /*
+        create surface
+    */
+    WN_VK_CHECK(glfwCreateWindowSurface(core.instance, window, NULL, &swapchain.surface));
+
+    /*
+        get surface info
+    */
+    VkSurfaceCapabilitiesKHR surface_caps;
+    WN_VK_CHECK(vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device.physical, swapchain.surface, &surface_caps));
+
+    uint32_t format_count = 0;
+    WN_VK_CHECK(vkGetPhysicalDeviceSurfaceFormatsKHR(device.physical, swapchain.surface, &format_count, NULL));
+    assert(format_count > 0);
+
+    VkSurfaceFormatKHR* surface_formats = NULL;
+    stbds_arrsetlen(surface_formats, format_count);
+    WN_VK_CHECK(vkGetPhysicalDeviceSurfaceFormatsKHR(device.physical, swapchain.surface, &format_count, surface_formats));
+
+    uint32_t present_mode_count = 0;
+    WN_VK_CHECK(vkGetPhysicalDeviceSurfacePresentModesKHR(device.physical, swapchain.surface, &present_mode_count, NULL));
+}
+
+// TODO: Find other queues
+WnQueueFamilyIndices wn_find_queue_families(void)
+{
+    WnQueueFamilyIndices qfi;
+    uint32_t nqf = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device.physical, &nqf, NULL);
+    VkQueueFamilyProperties qfp[nqf];
+    vkGetPhysicalDeviceQueueFamilyProperties(device.physical, &nqf, qfp);
+
+    for (uint32_t i = 0; i < nqf; ++i) {
+        if (qfp[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            VkBool32 supports_present = false;
+            vkGetPhysicalDeviceSurfaceSupportKHR(device.physical, i, swapchain.surface, &supports_present);
+            // FIXME: Only selecting graphics + present queue, not seperate
+            if (!supports_present) {
+                continue;
+            }
+
+            qfi.graphics = i;
+            qfi.present = i;
+            qfi.supported |= VK_QUEUE_GRAPHICS_BIT;
+            break;
+        }
+    }
+
+    if (!qfi.supported & VK_QUEUE_GRAPHICS_BIT) {
+        log_fatal("No graphics/present queue found");
+        exit(1);
+    }
+
+    return qfi;
+}
+
+void wn_init_vulkan(GLFWwindow* window)
 {
     VkApplicationInfo app_info = {
         .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
@@ -28,23 +119,27 @@ void wn_init_vulkan(void)
         .apiVersion = VK_API_VERSION,
     };
 
+    uint32_t ext_count = 0;
+    const char** glfw_exts = glfwGetRequiredInstanceExtensions(&ext_count);
     const char** exts = NULL;
-    arrput(exts, VK_KHR_SURFACE_EXTENSION_NAME);
-    arrput(exts, VK_KHR_XCB_SURFACE_EXTENSION_NAME);
+
+    for (uint32_t i = 0; i < ext_count; i++) {
+        stbds_arrput(exts, glfw_exts[i]);
+    }
 
     const char** layers = NULL;
     VkInstanceCreateInfo instance_info = {
         .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
         .pApplicationInfo = &app_info,
-        .enabledExtensionCount = arrlen(exts),
+        .enabledExtensionCount = stbds_arrlen(exts),
         .ppEnabledExtensionNames = exts,
-        .enabledLayerCount = arrlen(layers),
+        .enabledLayerCount = stbds_arrlen(layers),
         .ppEnabledLayerNames = layers,
     };
 
 #ifdef DEBUG_ENABLED
-    arrput(exts, VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-    arrput(layers, "VK_LAYER_KHRONOS_validation");
+    stbds_arrput(exts, VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    stbds_arrput(layers, "VK_LAYER_KHRONOS_validation");
 
     VkDebugUtilsMessengerCreateInfoEXT debug_messenger_info = {
         .sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
@@ -58,23 +153,96 @@ void wn_init_vulkan(void)
     };
 
     // FIXME: This seems hacky
-    instance_info.enabledExtensionCount = arrlen(exts);
+    instance_info.enabledExtensionCount = stbds_arrlen(exts);
     instance_info.ppEnabledExtensionNames = exts;
-    instance_info.enabledLayerCount = arrlen(layers);
+    instance_info.enabledLayerCount = stbds_arrlen(layers);
     instance_info.ppEnabledLayerNames = layers;
     instance_info.pNext = &debug_messenger_info;
 #endif
 
-    VK_CHECK_RESULT(vkCreateInstance(&instance_info, NULL, &core.instance));
+    /*
+        instance
+    */
+    WN_VK_CHECK(vkCreateInstance(&instance_info, NULL, &core.instance));
 
 #ifdef DEBUG_ENABLED
     PFN_vkCreateDebugUtilsMessengerEXT debug_messenger_pfn
         = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(core.instance, "vkCreateDebugUtilsMessengerEXT");
-    assert(debug_messenger_pfn != NULL);
-    VK_CHECK_RESULT(debug_messenger_pfn(core.instance, &debug_messenger_info, NULL, &core.debug_messenger));
+    assert(debug_messenger_pfn);
+    WN_VK_CHECK(debug_messenger_pfn(core.instance, &debug_messenger_info, NULL, &core.debug_messenger));
 #else
     core.debug_messenger = NULL;
 #endif
+
+    /*
+        physical device
+    */
+    uint32_t device_count = 0;
+    WN_VK_CHECK(vkEnumeratePhysicalDevices(core.instance, &device_count, NULL));
+    assert(device_count > 0);
+
+    VkPhysicalDevice physical_devices[device_count];
+    VkResult err = vkEnumeratePhysicalDevices(core.instance, &device_count, physical_devices);
+
+    // FIXME: make macro
+    if (err) {
+        log_error("Could not enumerate physical devices\n");
+        exit(err);
+    }
+
+    // FIXME: Just picking first device rn
+    device.physical = physical_devices[0];
+    vkGetPhysicalDeviceProperties(device.physical, &device.properties);
+    vkGetPhysicalDeviceFeatures(device.physical, &device.features);
+
+    /*
+        swapchain
+    */
+    wn_init_swapchain(window);
+
+    /*
+        logical device
+    */
+
+    // queue infos
+    device.queue_family_indices = wn_find_queue_families();
+    float* queue_prios = NULL;
+    stbds_arrput(queue_prios, 1.0f);
+
+    // FIXME: fix when supporting seperate graphics/present queues
+    VkDeviceQueueCreateInfo queue_info = {
+        .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+        .queueCount = stbds_arrlen(queue_prios),
+        .queueFamilyIndex = device.queue_family_indices.graphics,
+        .pQueuePriorities = queue_prios,
+    };
+
+    VkPhysicalDeviceFeatures *enabled_features = NULL;
+    const char** device_exts = NULL;
+    stbds_arrput(device_exts, VK_KHR_SWAPCHAIN_EXTENSION_NAME);
+
+    // device
+    VkDeviceCreateInfo device_info = {
+        .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+        .pQueueCreateInfos = &queue_info,
+        .queueCreateInfoCount = 1,
+        .pEnabledFeatures = enabled_features,
+        .ppEnabledExtensionNames = device_exts,
+        .enabledExtensionCount = stbds_arrlen(device_exts),
+#ifdef DEBUG_ENABLED
+        .enabledLayerCount = stbds_arrlen(layers),
+        .ppEnabledLayerNames = layers,
+#else
+        .enabledLayerCount = 0,
+        .ppEnabledLayerNames = NULL,
+#endif
+    };
+
+    WN_VK_CHECK(vkCreateDevice(device.physical,&device_info, NULL, &device.logical));
+
+    // device queues
+    vkGetDeviceQueue(device.logical, device.queue_family_indices.graphics, 0, &device.queues.graphics);
+    vkGetDeviceQueue(device.logical, device.queue_family_indices.present, 0, &device.queues.present);
 }
 
 // TODO
@@ -83,11 +251,13 @@ void wn_destroy(void)
 #ifdef DEBUG_ENABLED
     PFN_vkDestroyDebugUtilsMessengerEXT messenger_destroyer
         = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(core.instance, "vkDestroyDebugUtilsMessengerEXT");
-    if (messenger_destroyer != NULL) {
+    if (messenger_destroyer) {
         messenger_destroyer(core.instance, core.debug_messenger, NULL);
     } else {
     }
 #endif
+    vkDestroySurfaceKHR(core.instance, swapchain.surface, NULL);
+    vkDestroyDevice(device.logical, NULL);
     vkDestroyInstance(core.instance, NULL);
 }
 
@@ -98,7 +268,17 @@ int main(void)
 #else
     log_set_level(LOG_ERROR);
 #endif
-    wn_init_vulkan();
+
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "whynot", NULL, NULL);
+
+    wn_init_vulkan(window);
     wn_destroy();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
--- src/util.h
+++ src/util.h
@@ -3,12 +3,12 @@
 #include "log.h"
 #include <vulkan/vulkan.h>
 
-#define VK_CHECK_RESULT(f)                                                                                             \
+#define WN_VK_CHECK(f)                                                                                           \
     {                                                                                                                  \
         VkResult res = (f);                                                                                            \
         if (res != VK_SUCCESS) {                                                                                       \
-            log_fatal("Fatal: VkResult is \"%d\" in %s at line %d\n", res, __FILE__, __LINE__);                        \
-            assert(res == VK_SUCCESS);                                                                                 \
+            log_fatal("VkResult is \"%s\" in %s at line %d\n", wn_vk_result_to_string(res), __FILE__, __LINE__);                               \
+            assert(res);                                                                                               \
         }                                                                                                              \
     }
 
@@ -25,8 +25,8 @@ VKAPI_ATTR VkBool32 VKAPI_CALL wn_util_debug_message_callback(VkDebugUtilsMessag
         message_type = "PERFORMANCE";
     }
 
-    const char* message_id_name = pCallbackData->pMessageIdName != NULL ? pCallbackData->pMessageIdName : "";
-    const char* message = pCallbackData->pMessage != NULL ? pCallbackData->pMessage : "";
+    const char* message_id_name = pCallbackData->pMessageIdName ? pCallbackData->pMessageIdName : "";
+    const char* message = pCallbackData->pMessage ? pCallbackData->pMessage : "";
 
     if (messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT) {
         log_info("%s [%s (%d)] : %s\n", message_type, message_id_name, pCallbackData->messageIdNumber, message);
@@ -36,4 +36,52 @@ VKAPI_ATTR VkBool32 VKAPI_CALL wn_util_debug_message_callback(VkDebugUtilsMessag
         log_error("%s [%s (%d)] : %s\n", message_type, message_id_name, pCallbackData->messageIdNumber, message);
     }
     return VK_FALSE;
+}
+
+char* wn_vk_result_to_string(VkResult result) {
+    switch (result) {
+#define TO_STR(s) case VK_ ##s: return #s
+    TO_STR(SUCCESS);
+    TO_STR(NOT_READY);
+    TO_STR(TIMEOUT);
+    TO_STR(EVENT_SET);
+    TO_STR(EVENT_RESET);
+    TO_STR(INCOMPLETE);
+    TO_STR(ERROR_OUT_OF_HOST_MEMORY);
+    TO_STR(ERROR_OUT_OF_DEVICE_MEMORY);
+    TO_STR(ERROR_INITIALIZATION_FAILED);
+    TO_STR(ERROR_DEVICE_LOST);
+    TO_STR(ERROR_MEMORY_MAP_FAILED);
+    TO_STR(ERROR_LAYER_NOT_PRESENT);
+    TO_STR(ERROR_EXTENSION_NOT_PRESENT);
+    TO_STR(ERROR_FEATURE_NOT_PRESENT);
+    TO_STR(ERROR_INCOMPATIBLE_DRIVER);
+    TO_STR(ERROR_TOO_MANY_OBJECTS);
+    TO_STR(ERROR_FORMAT_NOT_SUPPORTED);
+    TO_STR(ERROR_FRAGMENTED_POOL);
+    TO_STR(ERROR_UNKNOWN);
+    TO_STR(ERROR_OUT_OF_POOL_MEMORY);
+    TO_STR(ERROR_INVALID_EXTERNAL_HANDLE);
+    TO_STR(ERROR_FRAGMENTATION);
+    TO_STR(ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS);
+    TO_STR(ERROR_SURFACE_LOST_KHR);
+    TO_STR(ERROR_NATIVE_WINDOW_IN_USE_KHR);
+    TO_STR(SUBOPTIMAL_KHR);
+    TO_STR(ERROR_OUT_OF_DATE_KHR);
+    TO_STR(ERROR_INCOMPATIBLE_DISPLAY_KHR);
+    TO_STR(ERROR_VALIDATION_FAILED_EXT);
+    TO_STR(ERROR_INVALID_SHADER_NV);
+    TO_STR(ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT);
+    TO_STR(ERROR_NOT_PERMITTED_EXT);
+    TO_STR(ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT);
+    TO_STR(THREAD_IDLE_KHR);
+    TO_STR(THREAD_DONE_KHR);
+    TO_STR(OPERATION_DEFERRED_KHR);
+    TO_STR(OPERATION_NOT_DEFERRED_KHR);
+    TO_STR(PIPELINE_COMPILE_REQUIRED_EXT);
+    TO_STR(RESULT_MAX_ENUM);
+#undef TO_STR
+        default:
+        return "UNKNOWN_ERROR";
+   }
 }
